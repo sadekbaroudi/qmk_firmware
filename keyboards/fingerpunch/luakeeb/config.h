caaa896da466dce434ba03c6f0cbcd58e2072d65
Michael Limiero <mike5713@gmail.com>
2022-12-12 20:59:18 -0500

    Fixes for luakeeb with promicro_rp2040

--- keyboards/fingerpunch/luakeeb/config.h
+++ keyboards/fingerpunch/luakeeb/config.h
@@ -23,8 +23,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define VENDOR_ID       0xFEFE
 #define PRODUCT_ID      0x7178
-#define MANUFACTURER    sadekbaroudi
-#define PRODUCT         sadekbaroudi luakeeb
+#define MANUFACTURER    "sadekbaroudi"
+#define PRODUCT         "luakeeb"
 
 /* key matrix size */
 // Rows are doubled-up
