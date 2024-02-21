cd894b0fbda2531fbed3118bfb5ded42017bf939
Sadek Baroudi <sadekbaroudi@gmail.com>
2024-02-21 03:29:37 -0800

    finally found ideal settings for trackpad cover

---
+++ keyboards/fingerpunch/vulpes_minora/xivik/keymaps/sadekbaroudi/config.h
@@ -0,0 +1,5 @@
+// As of writing this code, default values are MXT_GAIN = 4, and MXT_TOUCH_THRESHOLD = 18
+// The settings below are optimized for this trackpad cover:
+// https://github.com/sadekbaroudi/vulpes-minora/blob/master/case/mx/vmin-vik-peacock-cover.stl
+#define MXT_GAIN 4
+#define MXT_TOUCH_THRESHOLD 12
